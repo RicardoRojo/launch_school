7e08d09d925867a1337f56f289e267693c465d77
Ricardo <ricardorojo@netscape.net>
2016-01-12 14:42:11 -0500

    refactor robot

--- weekly_challenges/robot_simulator/simulator.rb
+++ weekly_challenges/robot_simulator/simulator.rb
@@ -2,10 +2,9 @@
 class Robot
   attr_accessor :direction
   attr_accessor :position
+  DIRECTIONS = [:north, :east, :south, :west]
 
-  def initialize
-    
-  end
+  def initialize; end
 
   def orient(direction)
     fail ArgumentError, 'uh, me no compute' if direction == :crood
@@ -13,101 +12,61 @@ class Robot
   end
 
   def bearing
-    @direction
+    direction
   end
 
   def turn_right
-    if direction == :north
-      self.direction = :east
-    elsif direction == :east
-      self.direction = :south
-    elsif direction == :south
-      self.direction = :west
-    elsif direction == :west
-      self.direction = :north
-    end
+    self.direction = DIRECTIONS[(DIRECTIONS.find_index(direction) + 1) % 4]
   end
 
   def turn_left
-    if direction == :north
-      self.direction = :west
-    elsif direction == :west
-      self.direction = :south
-    elsif direction == :south
-      self.direction = :east
-    elsif direction == :east
-      self.direction = :north
-    end    
+    self.direction = DIRECTIONS[(DIRECTIONS.find_index(direction) - 1) % 4]
   end
 
   def advance
-    if direction == :north
+    case direction
+    when :north
       position['y'] += 1
-    elsif direction == :east
-      position['x'] += 1
-    elsif direction == :south
+    when :south
       position['y'] -= 1
-    elsif direction == :west
+    when :east
+      position['x'] += 1
+    when :west
       position['x'] -= 1
     end
   end
 
-  def at(x,y)
-    @position = {'x' => x, 'y' => y}
+  def at(x, y)
+    @position = { 'x' => x, 'y' => y }
   end
 
   def coordinates
-    self.position.values
+    position.values
   end
 end
 
 # class simulator
 class Simulator
-  attr_reader :instruction
-  INSTRUCTIONS = {'L' => :turn_left, 'R' => :turn_right, 'A' => :advance}
+  INSTRUCTIONS = { 'L' => :turn_left, 'R' => :turn_right, 'A' => :advance }
 
-  def initialize
-  end
+  def initialize; end
 
   def instructions(instructions)
-    instruction_array = []
-    instructions.chars.each do |instruction|
-      instruction_array << INSTRUCTIONS.select {|key,val| key == instruction}.values
-    end
-    instruction_array.flatten
+    instructions.chars.inject([]) { |a, e| a + get_instruction(e) }
   end
 
-  def place(robot,options = {})
-    robot.at(options[:x],options[:y])
+  def get_instruction(instruction)
+    INSTRUCTIONS.select { |key, _| key == instruction }.values
+  end
+
+  def place(robot, options = {})
+    robot.at(options[:x], options[:y])
     robot.orient(options[:direction])
   end
 
-  def evaluate(robot,orders)
-    inst = self.instructions(orders)
-
-    inst.each do |instruction|
+  def evaluate(robot, orders)
+    instructions(orders).each do |instruction|
       robot.send(instruction)
     end
   end
 end
-# robot can turn left or right
-# robot can advance
-# robot is in a grid facing a direction
-  # posible directions are cardinal points (N,S,W,E)
-  # coordinates in format {3,5}
-
-  # # 123456789
-  #           8
-  #           7
-  #           6
-  #           5
-  #           4
-  #           3
-  #           2
-  #           1
-  # [x,y] [->,!]
-  # if north and moves y += 1
-  # elsif west and moves x -= 1
-  # elsif east and moves x += 1
-  # elsif south and moves y -= 1
-      
